1a2e8e692e1063aadc28c769bac2159966c111b5
Jerry Schneider <jerry@iplantcollaborative.org>
2011-04-07 09:40:32 -0700

    cleaning up docs and code

--- src/MongoDBLibrary/connection_manager.py
+++ src/MongoDBLibrary/connection_manager.py
@@ -13,7 +13,7 @@
 #  limitations under the License.
 
 #import ConfigParser
-import pymongo
+#import pymongo
 
 class ConnectionManager(object):
     """
@@ -26,63 +26,20 @@ class ConnectionManager(object):
         """
         self._dbconnection = None
         
-    def connect_to_dbapi2(self, dbapiModuleName, *args):
-    #class pymongo.connection.Connection([host='localhost'[, port=27017[, pool_size=None[, auto_start_request=None[, timeout=None[, slave_okay=False[, network_timeout=None[, document_class=dict[, tz_aware=False]]]]]]]]])
-        """
-        Loads the DB API 2.0 module with the given `dbapiModuleName`, then uses
-        it to connect to the database using `args`. The arguments depend on the
-        dbapiModule which is used for the connection.
-        Example usage:
-        | # Using psycopg2, specify postgres database name, user and password |
-        | Connect To Dbapi2 | psycopg2 | my_db | user | s3cr3t |
-        | # Using pymysql, specify mysql database host, user, password, db and port |
-        | Connect To Dbapi2 | pymysql | localhost | user | s3cr3t | my_db | 3306 |
-
-        | # Using jaydebeapi, set classpath, connect via jdbc driver in Python or Jython |
-        | Set Environment Variable | CLASSPATH | ${CURDIR}/../libraries/ojdbc14-10.2.0.3.0.jar |
-        | Connect To Dbapi2 | jaydebeapi | oracle.jdbc.driver.OracleDriver | jdbc:oracle:thin:@host:port | user | s3cr3t |
-
-        """
-        db_api_2 = __import__(dbapiModuleName);
-        # TODO: pass configfile as second arg dbConfigFile="./resources/db.cfg"?
-        # maybe use ConfigParser.SafeConfigParser({'bar': 'Life', 'baz': 'hard'})
-        # seems to be recommended by Python reference
-        # don't know how to handle config.items so that it can be passed as args
-        self._dbconnection = db_api_2.connection.Connection (*args);
-        
     def connect_to_mongodb(self, dbHost='localhost', dbPort=27017, dbPoolSize=None, dbAutoStart=None, dbTimeout=None, dbSlaveOkay=False, dbNetworkTimeout=None, dbDocClass=dict, dbTZAware=False):
-    #class pymongo.connection.Connection([host='localhost'[, port=27017[, pool_size=None[, auto_start_request=None[, timeout=None[, slave_okay=False[, network_timeout=None[, document_class=dict[, tz_aware=False]]]]]]]]])
+        #class pymongo.connection.Connection([host='localhost'[, port=27017[, pool_size=None[, auto_start_request=None[, timeout=None[, slave_okay=False[, network_timeout=None[, document_class=dict[, tz_aware=False]]]]]]]]])
         """
-        Loads the DB API 2.0 module with the given `dbapiModuleName`, then uses
-        it to connect to the database using `args`. The arguments depend on the
-        dbapiModule which is used for the connection.
+        Loads pymongo and connects to the MongoDB host using parameters submitted.
+        
         Example usage:
-        | # Using psycopg2, specify postgres database name, user and password |
-        | Connect To Dbapi2 | psycopg2 | my_db | user | s3cr3t |
-        | # Using pymysql, specify mysql database host, user, password, db and port |
-        | Connect To Dbapi2 | pymysql | localhost | user | s3cr3t | my_db | 3306 |
-
-        | # Using jaydebeapi, set classpath, connect via jdbc driver in Python or Jython |
-        | Set Environment Variable | CLASSPATH | ${CURDIR}/../libraries/ojdbc14-10.2.0.3.0.jar |
-        | Connect To Dbapi2 | jaydebeapi | oracle.jdbc.driver.OracleDriver | jdbc:oracle:thin:@host:port | user | s3cr3t |
-
+        | # To connect to foo.bar.org's MongoDB service on port 27017 |
+        | Connect To MongoDB | foo.bar.org | ${27017} |
+        
         """
         dbapiModuleName = 'pymongo'
         db_api_2 = __import__(dbapiModuleName);
-        # TODO: pass configfile as second arg dbConfigFile="./resources/db.cfg"?
-        # maybe use ConfigParser.SafeConfigParser({'bar': 'Life', 'baz': 'hard'})
-        # seems to be recommended by Python reference
-        # don't know how to handle config.items so that it can be passed as args
+        
         dbPort = int(dbPort)
-        #print "dbapiModuleName is    [%s]" % dbapiModuleName
-        #print "dbHost is             [%s]" % dbHost
-        #print "dbPort is             [%s]" % dbPort
-        #print "dbPoolSize is         [%s]" % dbPoolSize
-        #print "dbAutoStart is        [%s]" % dbAutoStart
-        #print "dbTimeout is          [%s]" % dbTimeout
-        #print "dbSlaveOkay is        [%s]" % dbSlaveOkay
-        #print "dbDocClass is         [%s]" % dbDocClass
-        #print "dbTZAware is          [%s]" % dbTZAware
         print "host is               [%s]" % dbHost
         print "port is               [%s]" % dbPort
         print "pool_size is          [%s]" % dbPoolSize
@@ -91,18 +48,15 @@ class ConnectionManager(object):
         print "slave_okay is         [%s]" % dbSlaveOkay
         print "document_class is     [%s]" % dbDocClass
         print "tz_aware is           [%s]" % dbTZAware
-
-        #self._dbconnection = db_api_2.connection.Connection ([host=dbHost[, port=dbPort[, pool_size=dbPoolSize[, auto_start_request=dbAutoStart[, timeout=dbTimeout[, slave_okay=dbSlaveOkay[, network_timeout=dbNetworkTimeout[, document_class=dbDocClass[, tz_aware=dbTZAware]]]]]]]]]);
+        
         self._dbconnection = db_api_2.connection.Connection (host=dbHost, port=dbPort, pool_size=dbPoolSize, auto_start_request=dbAutoStart, timeout=dbTimeout, slave_okay=dbSlaveOkay, network_timeout=dbNetworkTimeout, document_class=dbDocClass, tz_aware=dbTZAware);
         
     def disconnect_from_mongodb(self):
         """
-        Disconnects from the database.
+        Disconnects from the MongoDB server.
         
         For example:
-        | Disconnect From Database | # disconnects from current connection to the database | 
+        | Disconnect From MongoDB | # disconnects from current connection to the MongoDB server | 
         """
-        #self._dbconnection.close()
-        #self._dbconnection.end_request()
         self._dbconnection.disconnect()
         
