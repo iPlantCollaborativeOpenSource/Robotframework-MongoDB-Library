1e4144199c1e3d672ec59741789182df7ae9d7b2
Jerry Schneider <jerry@iplantcollaborative.org>
2013-03-29 09:20:08 -0700

    cleanup removing unneeded commented lines

--- setup.py
+++ setup.py
@@ -5,17 +5,13 @@
 from distutils.core import setup
 
 import sys, os
-#sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
 sys.path.insert(0, os.path.join('src','MongoDBLibrary'))
 
 from version import VERSION
 
-#from MongoDBLibrary import __version__
-
 def main():
     setup(name         = 'robotframework-mongodblibrary',
           version      = VERSION,
-          #version      = __version__,
           description  = 'Mongo Database utility library for Robot Framework',
           author       = 'Jerry Schneider',
           author_email = 'jerry@iplantcollaborative.org',
