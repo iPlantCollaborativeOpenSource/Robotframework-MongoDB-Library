994b7d3bdc1708dc9da3bb01a75c7b9370f16356
serhatbolsu <serhatbolsu@gmail.com>
2015-10-18 22:19:09 +0300

    'Update Many MongoDB Records' keyword added - Potential assignment bug fixed - PEP 8 - Documentation fixes

--- src/MongoDBLibrary/__init__.py
+++ src/MongoDBLibrary/__init__.py
@@ -8,9 +8,7 @@ class MongoDBLibrary(MongoConnectionManager, MongoQuery):
     MongoDB Library contains utilities meant for Robot Framework's usage.
     
     This can allow you to query your Mongo database after an action has been made to verify the results.
-    
-    
-    
+
     References:
     
      + PyMongo 3.0.3 Documentation - http://api.mongodb.org/python/3.0.3/
